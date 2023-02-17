0df2cd234b2a478ab665d2f6fc7d363f1964f605
mostafa-kamal <mmkamal021@gmail.com>
2023-02-17 21:54:17 +0600

    ready five task

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,9 +7,11 @@ const {
   getTask,
   updateTask,
   deleteTask,
+  // editTask,
 } = require('../controllers/tasks')
 
 router.route('/').get(getAllTasks).post(createTask)
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+// .put(editTask)
 
 module.exports = router
