5a6f06f326957efe933e9cb3c8a2a51da51ac30c
mostafa-kamal <mmkamal021@gmail.com>
2023-02-18 14:28:28 +0600

    commited

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/api/v1/tasks', tasks)
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
-// app,get('/api/v1/tasks') - get all the tasks
+// app.get('/api/v1/tasks') - get all the tasks
 // app.post('/api/v1/tasks') - create a new task
 // app.get('/api/v1/tasks/:id') - get single task
 // app.patch('/api/v1/tasks/:id') - update task
